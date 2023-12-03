9bab3743ac0dc0fe1d252439318cd750af3ec78f
Felipe <schloegl10@hotmail.com>
2023-12-03 20:20:19 -0300

    implementação dos modelos e controllers do backend

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,13 +1,12 @@
 import { Router } from "express";
-//import { EnrollProfundidade, GetProfundidade } from '../controllers/Profundidade.js';
-//import { EnrollRisco, GetRiscos } from '../controllers/Riscos.js';
+import { EnrollProfundidade, GetProfundidade } from '../controllers/Profundidade.js';
+import { EnrollRisco, GetRiscos } from '../controllers/Riscos.js';
 
 const router = Router();
 
-//Estrutura a ser criada
-//router.get('/getamostras', GetProfundidade);
-//router.post('/salvar', EnrollProfundidade);
-//router.get('/getriscos', GetRiscos);
-//router.post('/salvarriscos', EnrollRisco);
+router.get('/getamostras', GetProfundidade);
+router.post('/salvar', EnrollProfundidade);
+router.get('/getriscos', GetRiscos);
+router.post('/salvarriscos', EnrollRisco);
 
 export { router };
