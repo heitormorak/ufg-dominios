c3f40fd4e8d9db67fe290001d7a2d17b290444b3
Heitor Rodrigues <heitor.rodrigues@discente.ufg.br>
2024-01-07 18:14:07 -0300

    feat: finishing login - frontend

--- frontend/src/components/login/Login.jsx
+++ frontend/src/components/login/Login.jsx
@@ -9,34 +9,43 @@ const Login = () => {
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const navigate = useNavigate();
-    const server = 'http://localhost:8080';
+    const server = 'http://localhost:3333';
 
-    async function Auth() {
-        const body = {
-            username: username,
-            password: password
-        }
-        const response = await fetch(`${server}/login`, {
-            method: "POST",             
-            headers:{
-                accept : "application/json",
-                "content-type": "application/json"
-            },
-            body: JSON.stringify(body)            
-        })
-        if (response.status >= 200 && response.status <=300){
-            const body = await response.json()
-            navigate('/risco');       
-        } else {
+    async function Auth(event) {
+        event.preventDefault();
+        try {
+            const body = {
+                username: username,
+                password: password
+            };
+            const response = await fetch(`${server}/login`, {
+                method: "POST",             
+                headers:{
+                    accept : "application/json",
+                    "content-type": "application/json"
+                },
+                body: JSON.stringify(body)            
+            });
+
+            const responseData = await response.json();
+    
+            if (response.status >= 200 && response.status <=300){
+                navigate('/risco');       
+            } else {
+                toast.error(responseData.msg);
+            }
+        } catch (error) {
+            console.error('Error:', error);
             toast.error("Error");
         }        
     }
+    
 
     return (
         
         <div className="login-container">
             <ToastContainer />
-            <form className="login-form" onSubmit={Auth()}>
+            <form className="login-form">
                 <h1>Login</h1>
                 <div className="input-group">
                     <label htmlFor="username">Usuário</label>
@@ -57,7 +66,7 @@ const Login = () => {
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-                <button type="submit">Entrar</button>
+                <button type="button" onClick={Auth}>Entrar</button>
                 <NavLink to='/cadastrousuario' activeStyle>
                     Cadastrar Usuário
                 </NavLink>
